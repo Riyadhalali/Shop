d37e2912c18c816459d9250493c608dc45298299
riyadhalali <eng.riyadalali93@gmail.com>
2022-03-26 10:48:11 +0300

    Creating a custom fonts and asset files also created a custom class for classes

--- app/src/main/java/com/riyad/shop/activities/MainActivity.kt
+++ app/src/main/java/com/riyad/shop/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.riyad.shop
+package com.riyad.shop.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.riyad.shop.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
